01d6ffd37fae7ec4b1d5246bde889c797ca995b1
乖乖 <baizuueui@gmail.com>
2024-12-31 15:28:29 +0800

    Update mygoogletrans.py

--- mygoogletrans.py
+++ mygoogletrans.py
@@ -11,7 +11,7 @@ import feedparser
 import requests
 from bs4 import BeautifulSoup
 from jinja2 import Template
-from pygtrans import translate
+from googletrans import Translator
 
 
 def get_md5_value(src):
